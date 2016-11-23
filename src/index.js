8a55d8b34a375ac7fa045215eb80322e4234ee9e
stud-nilstes <nils.tesdal@hist.no>
2016-11-23 22:22:50 +0100

    Bare react-klient2

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+var data = [
+  {"navn":"Hei heisen", "adresse":"Gata 1"},
+  {"navn":"Hei heisen2", "adresse":"Gata 2"}
+];
 ReactDOM.render(
-  <App />,
+  <App>{data}</App>,
   document.getElementById('root')
 );
