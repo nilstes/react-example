34f1728cc7b5b22b042ff63ea12797891098a8fe
stud-nilstes <nils.tesdal@hist.no>
2016-11-23 22:29:32 +0100

    la til node server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '.', 'build', 'index.html'));
+});
+app.use(express.static(path.resolve(__dirname, '.', 'build')));
+
+var server = app.listen(8080, () => {
+  console.log('Kjører server på port 8080');
+});
