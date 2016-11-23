c7753a8e09399ebb0283de14414f0635611b1b47
stud-nilstes <nils.tesdal@hist.no>
2016-11-23 22:37:51 +0100

    med rest-server

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
+
+require('./server/rest-endpoints.js')(app);
 
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '.', 'build', 'index.html'));
---
+++ server/rest-endpoints.js
@@ -0,0 +1,67 @@
+module.exports = function(app, loggedIn) {
+
+  var mysql = require('mysql');
+  var bodyParser = require('body-parser');
+  app.use(bodyParser.json()); // for parsing application/json
+
+  var pool = mysql.createPool({
+    connectionLimit : 2, // maks antall koblinger
+    host     : 'mysql.stud.iie.ntnu.no',
+    user     : 'nilstesd',
+    password : '*******',
+    database : 'nilstesd',
+    debug    :  false
+  });
+
+  app.get('/person', function (req, res) {
+    console.log('Got GET request');
+      pool.getConnection(function(err, connection) {
+      if (err) {
+        res.status(500); // Internal server error
+        res.json({"error" : "Error connecting to database: " + err});
+        return;
+      }
+      console.log('Connected to database...');
+
+      connection.query("select * from person", function(err, rows) {
+        connection.release(); // Legg tilbake i pool
+        if(!err) {
+          console.log(rows);
+          res.json(rows);
+        } else {
+          res.status(500);
+          res.json({"error" : "Error reading database: " + err});
+        }
+      });
+    });
+  });
+
+  app.post('/person', function (req, res) {
+    console.log('Got POST request');
+    pool.getConnection(function(err, connection) {
+      if (err) {
+        res.status(500); // Internal server error
+        res.json({"error" : "Error connecting to database: " + err});
+        return;
+      }
+      console.log('Connected to database...');
+
+      var values = [
+        req.body.navn,
+        req.body.adresse
+      ];
+      var sql = "insert into person (navn,adresse) values (?,?)";
+		
+      connection.query(sql, values, function(err) {
+        connection.release(); // Legg tilbake i pool
+        if(!err) {
+          console.log("Insert OK");
+          res.send("");
+        } else {
+          res.status(500);
+          res.json({"error" : "Error inserting into database: " + err});
+        }
+      });
+    });
+  });
+}
