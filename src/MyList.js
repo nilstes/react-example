60e6dc15762843b5fbd751cb51ef00e652f0418e
nilstes <nils.tesdal@gmail.com>
2017-10-06 14:49:00 +0200

    formatert html

--- src/MyList.js
+++ src/MyList.js
@@ -5,11 +5,23 @@ class MyList extends Component {
   render() {
     return (
       <table>
-		<thead><tr><th>Navn</th><th>Adresse</th><th>Alder</th></tr></thead>
+		<thead>
+          <tr>
+            <th>Navn</th>
+            <th>Adresse</th>
+            <th>Alder</th>
+          </tr>
+        </thead>
 		<tbody>
         {
           this.props.children.map(function(item) {
-            return <tr><td>{item.navn}</td><td>{item.adresse}</td><td>{item.alder}</td></tr>
+            return ( 
+              <tr>
+                <td>{item.navn}</td>
+                <td>{item.adresse}</td>
+                <td>{item.alder}</td>
+              </tr>
+            )
           })
         }
 	    </tbody>
